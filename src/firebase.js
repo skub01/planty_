f01ca7abc695188975d05c3c47ca7821e942e68b
Sarah Kublin <127888292+skub01@users.noreply.github.com>
2023-09-18 12:50:53 -0400

    firebase init

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,6 @@
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import {getStorage} from 'firebase/storage';
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -11,5 +13,7 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const storage=getStorage(app);
 
 export default app;
