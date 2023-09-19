49ca58204784f3a2689c83bba5f457a4927ab738
Sarah Kublin <127888292+skub01@users.noreply.github.com>
2023-09-19 10:45:59 -0400

    styling for auth components

--- src/features/auth/Login.js
+++ src/features/auth/Login.js
@@ -3,7 +3,6 @@ import { useNavigate } from "react-router-dom";
 import { auth, db } from "../../firebase";
 import { signInWithEmailAndPassword, signInWithPopup ,updateProfile,GoogleAuthProvider} from "firebase/auth";
 import { doc, setDoc,getDoc } from "firebase/firestore";
-import { Form, Button, Card, Alert, Container } from "react-bootstrap";
 
 const Login = () => {
   const navigate = useNavigate();
@@ -71,48 +70,32 @@ const Login = () => {
   };
 
   return (
-    <Container
-      fluid="lg"
-      style={{ paddingTop: "3rem" }}
-      className="d-flex justify-content-center"
-    >
-      <Card className="form-width" style={{ width: "30rem" }}>
-        <Card.Body>
-          <h2 className="form-name title mb-4 text-center">Log In</h2>
-          {error && <Alert variant="danger">{error}</Alert>}
-          <Form>
-            <Form.Group id="email" style={{ paddingTop: ".25rem" }}>
-              <Form.Label>Email</Form.Label>
-              <Form.Control type="email" ref={emailRef} required />
-            </Form.Group>
-            <Form.Group
+    <div className="auth-form">
+      <div>
+        <h2>Log In</h2>
+        <form>
+          <div>
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" required />
+          </div>
+          <div>
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
               id="password"
-              style={{ paddingBottom: "2rem", paddingTop: ".25rem" }}
-            >
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" ref={passwordRef} required />
-            </Form.Group>
-            <Button
-              variant="secondary"
-              onClick={handleEmailLogin}
-              disabled={loading}
-              className="w-100 mb-3"
-              type="submit"
-            >
-              Log In with Email
-            </Button>
-            <Button
-              variant="secondary"
-              onClick={handleGoogleLogin}
-              className="w-100 mb-3"
-              type="button"
-            >
-              Log In with Google
-            </Button>
-          </Form>
-        </Card.Body>
-      </Card>
-    </Container>
+              className="form-control"
+              required
+            />
+          </div>
+          <button type="submit"  onClick={handleEmailLogin}>
+            Log In with Email
+          </button>
+          <button type="button" onClick={handleGoogleLogin}>
+            Log In with Google
+          </button>
+        </form>
+      </div>
+    </div>
   );
 };
 
--- src/features/auth/Signup.js
+++ src/features/auth/Signup.js
@@ -1,12 +1,12 @@
 import React, { useRef, useState } from "react";
 import { auth, db } from "../../firebase";
-import { doc, setDoc ,getDoc} from "firebase/firestore";
+import { doc, setDoc, getDoc } from "firebase/firestore";
 import { useNavigate } from "react-router-dom";
 import {
   createUserWithEmailAndPassword,
   signInWithPopup,
   updateProfile,
-  GoogleAuthProvider
+  GoogleAuthProvider,
 } from "firebase/auth";
 import { Form, Button, Card, Alert, Container } from "react-bootstrap";
 
@@ -71,107 +71,62 @@ export default function Signup() {
     try {
       const { user } = await signInWithPopup(auth, googleProvider);
 
-    // You can customize the behavior after successful login here
-    console.log("Successfully signed in with Google:", user);
+      // You can customize the behavior after successful login here
+      console.log("Successfully signed in with Google:", user);
 
-    // If you want to check if the user is new or existing and perform additional actions, you can do so here
-    const userRef = doc(db, "users", user.uid);
-    const docSnap = await getDoc(userRef);
+      // If you want to check if the user is new or existing and perform additional actions, you can do so here
+      const userRef = doc(db, "users", user.uid);
+      const docSnap = await getDoc(userRef);
 
-    if (!docSnap.exists()) {
-      const name = user.displayName || "No Name";
+      if (!docSnap.exists()) {
+        const name = user.displayName || "No Name";
 
-      // Update the user's profile with their name (regardless of whether it's a new user)
-      await updateProfile(user, { displayName: name });
+        // Update the user's profile with their name (regardless of whether it's a new user)
+        await updateProfile(user, { displayName: name });
 
-      // Add the user's data to the "users" collection in Firestore
-      const userData = {
-        id: user.uid,
-        name: name,
-        email: user.email,
-      };
+        // Add the user's data to the "users" collection in Firestore
+        const userData = {
+          id: user.uid,
+          name: name,
+          email: user.email,
+        };
 
-      // Use setDoc to explicitly specify the document ID (user's uid) in the "users" collection
-      await setDoc(doc(db, "users", user.uid), userData);
-      navigate("/user-details");
-    } else {
-      // User is existing, perform any necessary actions for existing users
-      console.log("Existing user, performing login...");
-      navigate("/");
-    }
+        // Use setDoc to explicitly specify the document ID (user's uid) in the "users" collection
+        await setDoc(doc(db, "users", user.uid), userData);
+        navigate("/user-details");
+      } else {
+        // User is existing, perform any necessary actions for existing users
+        console.log("Existing user, performing login...");
+        navigate("/");
+      }
     } catch (err) {
       setError("Failed to sign in with Google: " + err.message);
     }
   };
 
   return (
-    <>
-      <Container
-        fluid="lg"
-        style={{ paddingTop: "3rem" }}
-        className="d-flex justify-content-center"
-      >
-        <Card className=" form-width" style={{ width: "30rem" }}>
-          <Card.Body>
-            <h2 className="form-name title mb-4 text-center">Sign Up</h2>
-            {error && <Alert variant="danger">{error}</Alert>}
-            <Form>
-              <Form.Group
-                id="name"
-                className="form-content"
-                style={{ paddingTop: ".25rem" }}
-              >
-                <Form.Label>Name</Form.Label>
-                <Form.Control type="text" ref={nameRef} required />
-              </Form.Group>
-              <Form.Group
-                id="email"
-                className="form-content"
-                style={{ paddingTop: ".25rem" }}
-              >
-                <Form.Label>Email</Form.Label>
-                <Form.Control type="email" ref={emailRef} required />
-              </Form.Group>
-              <Form.Group
-                id="password"
-                className="form-content"
-                style={{ paddingTop: ".25rem" }}
-              >
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" ref={passwordRef} required />
-              </Form.Group>
-              <Form.Group
-                id="password-confirm"
-                className="form-content"
-                style={{ paddingBottom: "2rem", paddingTop: ".25rem" }}
-              >
-                <Form.Label>Password Confirmation</Form.Label>
-                <Form.Control
-                  type="password"
-                  ref={passwordConfirmRef}
-                  required
-                />
-              </Form.Group>
-              <Button
-                variant="secondary"
-                onClick={handleSubmit}
-                disabled={loading}
-                className="w-100 mb-3"
-                type="submit"
-              >
-                Sign Up
-              </Button>
-            </Form>
-            <Button
-              variant="secondary"
-              onClick={signInWithGoogle}
-              className="w-100 mb-3"
-            >
-              Sign Up With Google
-            </Button>
-          </Card.Body>
-        </Card>
-      </Container>
-    </>
+    <div className="auth-form">
+      <h2>Sign Up</h2>
+      {error && <p style={{ color: "red" }}>{error}</p>}
+      <form>
+        <label>Name</label>
+        <input type="text" ref={nameRef} required />
+
+        <label>Email</label>
+        <input type="email" ref={emailRef} required />
+
+        <label>Password</label>
+        <input type="password" ref={passwordRef} required />
+
+        <label>Password Confirmation</label>
+        <input type="password" ref={passwordConfirmRef} required />
+
+        <button onClick={handleSubmit} disabled={loading} type="submit">
+          Sign Up
+        </button>
+      </form>
+
+      <button onClick={signInWithGoogle}>Sign Up With Google</button>
+    </div>
   );
 }
